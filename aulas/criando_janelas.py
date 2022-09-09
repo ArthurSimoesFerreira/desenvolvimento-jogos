174525339f9f8ca52735244c7478793f751f4eeb
ArthurSimoesFerreira <asferreira1002@gmail.com>
2022-09-09 10:10:38 -0300

    bola desenhada

--- aulas/criando_janelas.py
+++ aulas/criando_janelas.py
@@ -1,10 +1,16 @@
 from PPlay.window import*
-
+from PPlay.sprite import*
+from aulas.PPlay import sprite
 ## 1.1 - Creating a Window using (X, Y) as dimensions, where X is the ##
 ## width and Y the height ##
 ## Construtor da classe Window
-janela = Window(1000, 400)
+janela = Window(700, 700)
 janela.set_background_color(RGB=[0,128,0])
+
+bola = Sprite("assets/bolinha.png", 1)
+bola.x = 300
+bola.y = 312
 # Game Loop
 while(True):
     janela.update()
+    bola.draw
