1915673f4427f4b79ba6ab2312f3c885b12fedb3
ArthurSimoesFerreira <asferreira1002@gmail.com>
2022-09-09 10:43:51 -0300

    Pondo colisão entre a bolinha e a parede

--- aulas/criando_janelas.py
+++ aulas/criando_janelas.py
@@ -1,16 +1,26 @@
 from PPlay.window import*
 from PPlay.sprite import*
 
-## 1.1 - Creating a Window using (X, Y) as dimensions, where X is the ##
-## width and Y the height ##
-## Construtor da classe Window
+# Janela
 janela = Window(700, 700)
-janela.set_background_color(RGB=[100,100,100])
+janela.set_background_color(RGB=[0,0,0])
 
+# Bolinha
 bola = Sprite("bolinha.png", 1)
-bola.x = 300
-bola.y = 312
+bola.x = janela.width/2 - bola.width/2
+bola.y = janela.height/2 - bola.height/2
+velx = 0.5
+vely = 0.5
+
 # Game Loop
 while(True):
     janela.update()
+    janela.set_background_color(RGB=[0,0,0])
     bola.draw()
+    bola.move_x(velx) ## bola.x = bola.x + velx
+    bola.move_y(vely) ## bola.y = bola.y + vely
+    
+    if bola.x + bola.width == janela.width or bola.x == 0:
+        velx = -velx
+    elif bola.y + bola.height == janela.height or bola.y == 0:
+        vely = -vely
