b3b6582ce5b4117aed6b7ad43884b607b8cf11a2
ArthurSimoesFerreira <asferreira1002@gmail.com>
2022-10-07 16:31:39 -0300

    Implemetação da nave que anda para os lados, colidindo com as paredes.
    Implemetação do tiro, que sai da posição da nave,
    e outro tiro só pode ser dado quando o primeiro
    tiver feito todo o percurso

--- aulas/Space_Invaders/game.py
+++ aulas/Space_Invaders/game.py
@@ -9,8 +9,40 @@ def game():
     window = Window(900,600)
     keyboard = window.get_keyboard()
 
+    spaceShip = Sprite("spaceship.png",1)
+    spaceShip.x = window.width/2 - spaceShip.width/2
+    spaceShip.y = window.height - spaceShip.height
+    velxSpaceShip = 0.2
+
+    shot = Sprite("shot.png",1)
+    velyShot = -0.3
+    shotsFired = False
+
     while(True):
         window.update()
         window.set_background_color([0,0,0])
+        spaceShip.draw()
+
         if keyboard.key_pressed("ESC"):
             break
+
+        # SpaceShip
+        spaceShip.move_x(velxSpaceShip)
+        if (spaceShip.x + velxSpaceShip * window.delta_time()) <= 0:
+            velxSpaceShip = -velxSpaceShip
+        elif (spaceShip.x + spaceShip.width + velxSpaceShip * window.delta_time()) >= window.width:
+            velxSpaceShip = -velxSpaceShip
+        # Shot
+        if not(shotsFired):
+            if keyboard.key_pressed("SPACE"):
+                shot.x = (spaceShip.x + velxSpaceShip * window.delta_time()) + shot.width
+                shot.y = spaceShip.y
+                shotsFired = True
+        if shotsFired:
+            shot.draw()
+            shot.move_y(velyShot)
+            if (shot.y + velyShot * window.delta_time()) < 0:
+                shotsFired = False
+                shot.y = spaceShip.y
+            
+
